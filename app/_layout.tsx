9c0e9281c9a51785dc9a90b3645b94c2b584eb27
Abolfazl <abolfazljamshididev@gmail.com>
2026-02-11 18:53:12 +0330

    configure tailwind and nativewind for the project - #1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,27 @@
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import "./globals.css";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    PublicBlack: require("../assets/fonts/PublicSans-Black.ttf"),
-    PublicBlackItalic: require("../assets/fonts/PublicSans-BlackItalic.ttf"),
-    PublicBold: require("../assets/fonts/PublicSans-Bold.ttf"),
-    PublicBoldItalic: require("../assets/fonts/PublicSans-BoldItalic.ttf"),
-    PublicExtraBold: require("../assets/fonts/PublicSans-ExtraBold.ttf"),
-    PublicExtraBoldItalic: require("../assets/fonts/PublicSans-ExtraBoldItalic.ttf"),
-    PublicExtraLight: require("../assets/fonts/PublicSans-ExtraLight.ttf"),
-    PublicItalic: require("../assets/fonts/PublicSans-Italic.ttf"),
-    PublicLight: require("../assets/fonts/PublicSans-Light.ttf"),
-    PublicExtraLightItalic: require("../assets/fonts/PublicSans-LightItalic.ttf"),
-    PublicMedium: require("../assets/fonts/PublicSans-Medium.ttf"),
-    PublicMediumItalic: require("../assets/fonts/PublicSans-MediumItalic.ttf"),
-    PublicRegular: require("../assets/fonts/PublicSans-Regular.ttf"),
-    PublicSemiBold: require("../assets/fonts/PublicSans-SemiBold.ttf"),
-    PublicSemiBoldItalic: require("../assets/fonts/PublicSans-SemiBoldItalic.ttf"),
-    PublicThin: require("../assets/fonts/PublicSans-Thin.ttf"),
-    PublicThinItalic: require("../assets/fonts/PublicSans-ThinItalic.ttf"),
+    PublicBlack: require("../assets/fonts/public-sans/PublicSans-Black.ttf"),
+    PublicBlackItalic: require("../assets/fonts/public-sans/PublicSans-BlackItalic.ttf"),
+    PublicBold: require("../assets/fonts/public-sans/PublicSans-Bold.ttf"),
+    PublicBoldItalic: require("../assets/fonts/public-sans/PublicSans-BoldItalic.ttf"),
+    PublicExtraBold: require("../assets/fonts/public-sans/PublicSans-ExtraBold.ttf"),
+    PublicExtraBoldItalic: require("../assets/fonts/public-sans/PublicSans-ExtraBoldItalic.ttf"),
+    PublicExtraLight: require("../assets/fonts/public-sans/PublicSans-ExtraLight.ttf"),
+    PublicItalic: require("../assets/fonts/public-sans/PublicSans-Italic.ttf"),
+    PublicLight: require("../assets/fonts/public-sans/PublicSans-Light.ttf"),
+    PublicExtraLightItalic: require("../assets/fonts/public-sans/PublicSans-LightItalic.ttf"),
+    PublicMedium: require("../assets/fonts/public-sans/PublicSans-Medium.ttf"),
+    PublicMediumItalic: require("../assets/fonts/public-sans/PublicSans-MediumItalic.ttf"),
+    PublicRegular: require("../assets/fonts/public-sans/PublicSans-Regular.ttf"),
+    PublicSemiBold: require("../assets/fonts/public-sans/PublicSans-SemiBold.ttf"),
+    PublicSemiBoldItalic: require("../assets/fonts/public-sans/PublicSans-SemiBoldItalic.ttf"),
+    PublicThin: require("../assets/fonts/public-sans/PublicSans-Thin.ttf"),
+    PublicThinItalic: require("../assets/fonts/public-sans/PublicSans-ThinItalic.ttf"),
   });
 
   useEffect(() => {
